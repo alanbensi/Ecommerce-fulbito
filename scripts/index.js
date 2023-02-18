3693aa2d176c9b53b7ab36f7ca9464e7362e042d
Alan <alanbensi9@gmail.com>
2023-02-17 23:57:01 -0300

    Funcionalidad del carrito de compras desde js casi lista!

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,11 @@
 const contenedorProductos = document.getElementById("contenedorProductos")
+const contadorCarrito = document.getElementById ("contadorCarrito"); 
+const carritoOffcanvas = document.getElementById("carritoOffcanvas");
+const contenedorContadorCarrito = document.getElementById("contenedorContadorCarrito");
+const precioTotalCarrito = document.getElementById ("precioTotalCarrito");
+const terminarCompra = document.getElementById ("terminarCompra");
+const contador = document.createElement("p");
+
 
 
 const camisetas = [ 
@@ -15,9 +22,12 @@ const camisetas = [
     {id:11, nombre: "San Lorenzo de Almagro", imagen:"./imagenes/san-lorenzo.jpg", precio: 9000, cantidad: 1},
     {id:12, nombre: "Talleres de Cordoba", imagen:"./imagenes/talleres.jpg", precio: 7000, cantidad: 1}
 ]
+const carritoDeCompras = [];
+
 
 camisetas.forEach (item => {
-    contenedorProductos.innerHTML += 
+    const div = document.createElement("div");
+    div.innerHTML += 
     `
     <div class="flip-card">
         <div class="flip-card-inner">
@@ -32,4 +42,87 @@ camisetas.forEach (item => {
         </div>
     </div>
     `
+    contenedorProductos.appendChild(div);
+
+    const botonAgregarCarrito = document.getElementById(`camiseta${item.id}`);
+    botonAgregarCarrito.addEventListener ("click", ()=> {
+        agregarAlCarrito(item.id, carritoDeCompras);
+        agregarContadorCarrito();
+        mostrarCarrito();
+    })
 })
+
+const agregarAlCarrito = (productoSeleccionado, carrito)=> {
+    const productoExiste = carritoDeCompras.some (camiseta => camiseta.id === productoSeleccionado);
+    const productoElegido = camisetas.find (camiseta => camiseta.id === productoSeleccionado);
+    if (productoExiste) {
+        let precioInicial = productoElegido.precio;
+        productoElegido.cantidad++;
+        productoElegido.nuevoPrecio = productoElegido.cantidad * precioInicial;
+    } else {
+        carrito.push (productoElegido);
+        console.log ("Agregado con exito");
+        console.log (carrito);
+    }
+}
+
+const agregarContadorCarrito = ()=> {
+    if (carritoDeCompras.length !== 0) {
+        contenedorContadorCarrito.appendChild(contador);
+        contador.textContent = carritoDeCompras.length;
+        contador.classList.add("contadorCarrito");
+    } else {
+        contador.textContent ="";
+        contador.classList.remove ("contadorCarrito");
+    }
+}
+
+const mostrarCarrito = ()=>{
+    carritoOffcanvas.innerHTML="";
+    carritoDeCompras.forEach (producto => {
+        tr = document.createElement ("tr");
+        tr.classList.add("tablaProductos");
+        tr.innerHTML += 
+        `
+            <td>
+                <img src="${producto.imagen}" alt="${producto.nombre}">
+            </td>
+            <td class="infoProducto">Camiseta ${producto.nombre}</td>
+            <td class="infoProducto">${producto.cantidad}</td>
+            <td class="infoProducto">${producto.precio}</td>
+            <td class="infoProducto eliminarProducto">
+                <iconify-icon icon="material-symbols:delete-outline" class="deleteIconify" id="eliminar${producto.id}"></iconify-icon>
+            </td>
+        `
+
+        carritoOffcanvas.appendChild(tr)
+
+        const botonEliminar = document.getElementById(`eliminar${producto.id}`);
+        botonEliminar.addEventListener("click", ()=> {
+            eliminarProducto(producto.id)
+        })
+    })
+    const totalCarrito = carritoDeCompras.reduce ((acumulador,producto) => acumulador + producto.precio,0);
+    precioTotalCarrito.innerText =`Precio total: $${totalCarrito}`;
+}
+
+
+const eliminarProducto = (productoClickeado) => {
+    const productoEliminado = carritoDeCompras.find (camiseta =>camiseta.id === productoClickeado);
+    const index = carritoDeCompras.indexOf (productoEliminado);
+    carritoDeCompras.splice (index,1);
+    agregarContadorCarrito();
+    mostrarCarrito();
+}
+
+const vaciarCarrito = ()=> {
+    carritoDeCompras.innerHTML =[];
+    carritoOffcanvas.innerHTML =[];
+    contador.textContent ="";
+    contador.classList.remove ("contadorCarrito");
+}
+
+terminarCompra.addEventListener ("click", ()=> {
+    vaciarCarrito();
+})
+
